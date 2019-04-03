e812d4caf4962d4bdd3a6fa0bbb18ecb9a5afaf0
Evgeny Kushnir <keg1980@gmail.com>
2019-04-03 22:30:36 +0300

    HW4 is done

--- TODO List/Controllers/HomeController.cs
+++ TODO List/Controllers/HomeController.cs
@@ -3,14 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TODO_List.Models;
 
 namespace TODO_List.Controllers
 {
     public class HomeController : Controller
     {
+        static List<ToDoItem> TodoList = new List<ToDoItem>();
+
         public ActionResult Index()
         {
+            return View(TodoList);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ToDoItem item)
+        {
+            var lastItem = TodoList.LastOrDefault();
+            item.Id = lastItem == null ? 0: lastItem.Id+1;
+            TodoList.Add(item);
+            return RedirectToAction("Details", new { item.Id });
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(TodoList.Find(i => i.Id == id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(TodoList.Find(i=>i.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ToDoItem item)
+        {
+            var todoItem = TodoList.Find(i => i.Id == item.Id);
+            todoItem.Caption = item.Caption;
+            todoItem.Description = item.Description;
+            todoItem.IsDone = item.IsDone;
+            return RedirectToAction("Details", new { item.Id });
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(TodoList.Find(i => i.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(ToDoItem item)
+        {
+            TodoList.RemoveAll(i => i.Id == item.Id);
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
---
+++ TODO List/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TODO_List.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Caption { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
